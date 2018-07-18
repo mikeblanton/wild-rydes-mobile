93fb8af1d6b09f88dd987019c643a9f3e1f8f994
Michael Blanton <mblanton@gmail.com>
2018-07-18 11:33:29 -0700

    added lambda function

--- react-aws-app/src/App.js
+++ react-aws-app/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { withAuthenticator } from 'aws-amplify-react'
+import { API } from 'aws-amplify'
+
 import logo from './logo.svg';
 import './App.css';
 
+let apiName = 'PetAPI';
+let path = '/pets';
+
 class App extends Component {
+  state = {
+    pets: []
+  }
+
+  async componentDidMount() {
+    const data = await API.get(apiName, path)
+    console.log('data: ', data)
+    this.setState({
+      pets: data.data
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +31,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {
+          this.state.pets.map((pet, index) => (
+            <h2 key={index}>{pet}</h2>
+          ))
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App)
--- react-aws-app/src/index.js
+++ react-aws-app/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
